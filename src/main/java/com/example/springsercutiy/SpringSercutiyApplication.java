659046f910b0de2103b2a4cc474a23cde3675b53
quocanh07 <anhquoc71202@gmail.com>
2023-02-10 15:15:17 +0700

    Initial commit

---
+++ src/main/java/com/example/springsercutiy/SpringSercutiyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsercutiy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSercutiyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSercutiyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsercutiy/SpringSercutiyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsercutiy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSercutiyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
